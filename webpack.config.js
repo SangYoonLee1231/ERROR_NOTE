ccce241c2f1cac67acf79251685a68b4e310a86a
SangYoonLee <prestro1231@hufs.ac.kr>
2022-12-16 15:01:01 +0900

    Revert "ADD: 초기 세팅 webpack 설정 수정"

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,22 @@
 // webpack.config.js
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (_env, argv) => {
-  const isProd = argv.mode === 'production';
-  // const isDev = !isProd;
-
-  return {
-    entry: path.resolve(__dirname, 'src/index.jsx'),
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
-        {
-          test: /\.css$/i,
-          use: [
-            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
-          ],
-        },
-        {
-          test: /\.s[ac]ss$/,
-          use: [
-            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                // 2 => postcss-loader, sass-loader
-                importLoaders: 2,
-              },
-            },
-            'resolve-url-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'assets/css/[name].[contenthash:8].css',
-        chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css',
-      }),
+      },
     ],
-  };
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
